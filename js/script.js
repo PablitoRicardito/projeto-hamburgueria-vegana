b8eb8788b6762bb29c3cf4f4fdc97dba7050f40c
Cristiano Reis <117485646+Akumanoir@users.noreply.github.com>
2023-10-17 23:51:14 -0300

    added cart function

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 import Cardapio from "./cardapio/cardapio.js"
 const section_content = document.querySelector("main > section");
 const navigationButton = [...document.querySelectorAll(".nav")];
+let carrinho = new Array();
 
 function inicio() {
     section_content.innerHTML = ""
@@ -36,15 +37,13 @@ function cardapio() {
             cardapio_modal(Cardapio[index]);
         });
     })
-    console.log(cardapioButton);
 }
 
 function cardapio_modal(lanche) {
     const modalContainer = document.querySelector("div#modal_container");
     modalContainer.classList.add("modal_container");
 
-    modalContainer.innerHTML = 
-    `
+    modalContainer.innerHTML = `
     <section class="modal">
         <div class="left_container">
             <img src="${lanche.imagem}" alt="imagem de hamburguer">
@@ -58,14 +57,26 @@ function cardapio_modal(lanche) {
             </div>
             <div class="middle">
                 <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non illum quasi odio cupiditate. Eos rerum rem, repellendus ratione tempore neque doloremque earum ducimus voluptates! Iusto dignissimos iure omnis consequatur! Porro? Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt molestias architecto quos necessitatibus veritatis totam vel possimus sapiente, quas iste rerum maiores dolorum, sequi quasi mollitia voluptates magni ea explicabo.</p>
-                <ul></ul>
+                <ul style="list-style-position: inside;">
+                    ${listMaping(lanche.ingredientes)}
+                </ul>
+            </div>
+            <div class="bottom">
+                <span>R$${lanche.valor}</span>
+                <button type="button" class="added_cart cart_button">Adicionar ao carrinho</button>
             </div>
         </div>
     </section>
-    `
+    `;
+
+    const carrinhoButton = document.querySelector("button.added_cart");
+    const { nome, imagem, valor} = lanche // pega sómente esses atributos do cardapio que chamei de lanche nessa função, por causa do parametro.
+    carrinhoButton.addEventListener("click", () => {
+        addCartItem(nome, imagem, valor);
+        console.log(carrinho);
+    });
 
     const modal = document.querySelector("section.modal");
-    console.log(modal)
     const closeButton = document.querySelectorAll("div.close_button");
     closeButton.forEach((button) => {
         button.addEventListener("click", () => {
@@ -75,7 +86,52 @@ function cardapio_modal(lanche) {
     });
 }
 
+function listMaping(list) {
+    let listTag = "";
+    list.map((element) => {
+      listTag += `<li>${element}</li>`;
+    });
+    return listTag;
+}
+
+function addCartItem(name, image, value) {
+    let cartValues = {name: name, image:image, value: value};
+    carrinho.push(cartValues);
+    return carrinho;
+}
+
+function viewCartItens() {
+    section_content.innerHTML = ""
+    if(section_content.classList.contains("cardapio_container")){
+        section_content.classList.remove("cardapio_container");
+    }
+    if (carrinho.length != 0) {
+        carrinho.map((itens) => {
+            section_content.innerHTML += `<div class="carrinho_container">
+                <h3>${itens.name}</h3><img src="${itens.image}" alt="imagem de hamburguer"><p>R$${itens.value}</p><button type="button" class="cart_button remove_item">Remover item</button>
+            </div>`;
+        });
+        const removeItem = [...document.querySelectorAll("button.remove_item")];
+        const carrinhoContainer = [...document.querySelectorAll(
+          ".carrinho_container"
+        )];
+        removeItem.forEach((element, index) => {
+            element.addEventListener("click", () => {
+              removeCartItem(index, carrinhoContainer);
+            });
+        })
+    } else {
+        section_content.innerHTML = "Não há itens no carrinho"
+    }
+}
+
+function removeCartItem(index, container) {
+    carrinho.splice(index, 1);
+    section_content.removeChild(container[index])
+}
+
 
 document.body.onload = inicio()
 navigationButton[0].addEventListener("click", inicio);
 navigationButton[1].addEventListener("click", cardapio);
+navigationButton[2].addEventListener("click", viewCartItens);
